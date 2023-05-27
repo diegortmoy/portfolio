4861c411cfcc3c1197cd526bcabb0e7c3ae53ca4
Diego <diegortmoy@users.noreply.github.com>
2023-05-27 16:27:49 -0600

    contact.php

--- contact.php
+++ contact.php
@@ -1,16 +1,15 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $nombre = $_POST['nombre'];
-  $email = $_POST['email'];
-  $asunto = $_POST['asunto'];
-  $mensaje = $_POST['mensaje'];
+  $name = $_POST['name'];
+  $mail = $_POST['mail'];
+  $message = $_POST['message'];
   
   // Configura los detalles del correo electrónico
   $destinatario = 'diegortmoy@gmail.com'; 
-  $cabeceras = "From: $nombre <$email>" . "\r\n";
+  $cabeceras = "From: $name <$mail>" . "\r\n";
   
   // Envía el correo electrónico
-  if (mail($destinatario, $asunto, $mensaje, $cabeceras)) {
+  if (mail($destinatario, $message, $cabeceras)) {
     echo "Correo enviado exitosamente";
   } else {
     echo "Error al enviar el correo";
